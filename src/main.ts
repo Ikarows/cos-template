eac6eff483afb826c3b26d199d38badd3849fae5
Ikarows <1254803307@qq.com>
2022-11-08 00:55:31 +0800

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import {createApp} from 'vue'
-import './style.css'
 import App from './App.vue'
 import router from './router/index'
+import {createPinia} from 'pinia'
+const store = createPinia()
+import './style.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import {defineStore} from "pinia";
+ 
+export const userIndex = defineStore('index', {
+    state: () => {
+        return {
+           demo: '1111'
+        }
+    },
+
+    getters: {
+
+    },
+
+    actions: {
+ 
+    }
+})
