d9fae663ce9184f8547717a823e6e6898f045dd7
Ikarows <1254803307@qq.com>
2022-12-08 10:33:17 +0800

    优化eslint

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,7 @@
 </template>
 
 <script setup lang="ts">
-  import { ref } from 'vue'
-  import type { Ref } from 'vue'
   import HelloWorld from '@/components/HelloWorld.vue'
-
-  const arr = [{ id: 1, name: 111 }]
-
-  interface Fors {
-    id: number
-    name: number | string
-  }
-
-  const num: Ref<number> = ref(0)
-  console.log(num)
-
-  arr.forEach((v: Fors, i: number) => {
-    console.log(v.id, i)
-  })
 </script>
 
 <style lang="scss" scoped></style>
